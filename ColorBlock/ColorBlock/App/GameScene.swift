3658fe6beb417f16b729c86342d8f8572030b33f
Yuning Zhou <yuning.zhou@stern.nyu.edu>
2020-05-03 19:27:02 -0400

    added block

--- ColorBlock/ColorBlock/App/GameScene.swift
+++ ColorBlock/ColorBlock/App/GameScene.swift
@@ -18,5 +18,17 @@ class GameScene: SKScene {
    
     func layoutScene(){
         backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 189/255, alpha: 1.0)
+        spawnBlocks()
+    }
+    
+    func spawnBlocks(){
+        let block = SKSpriteNode(imageNamed: "block")
+        
+        block.color = UIColor(red: 0, green: 1.0, blue: 0, alpha: 1.0)
+        
+        block.size = CGSize(width: frame.size.width/7, height: frame.size.width/7)
+        
+        block.position = CGPoint(x: frame.midX, y: frame.maxY - block.size.width)
+        addChild(block)
     }
 }

85446772698a28362e0a7c36f21c0a66221c1d4a
Yuning Zhou <yuning.zhou@stern.nyu.edu>
2020-05-06 23:29:37 -0400

    added sound effects

--- ColorBlock/ColorBlock/App/SoundViewController.swift
+++ ColorBlock/ColorBlock/App/SoundViewController.swift
@@ -10,21 +10,65 @@ import UIKit
 
 class SoundViewController: UIViewController {
 
+    var initialValue: Int!
+    @IBOutlet weak var defaultButton: UIButton!
+    @IBOutlet weak var bubbleButton: UIButton!
+    @IBOutlet weak var alienButton: UIButton!
+    var selected: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        var sound = 0
+        let defaults = UserDefaults.standard
+             
+            
+        if (defaults.string(forKey: "sound") != nil){
+                sound = Int(defaults.string(forKey: "sound")!)!
+            }
+            
+        switch sound
+        {
+            case 0:
+                selected = defaultButton
+            case 1:
+                selected = bubbleButton
+            case 2:
+                selected = alienButton
+            default:
+                break
+        }
+            
+        selected.backgroundColor = .red
+        
+        
+        initialValue = sound
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func colorChanged(sender: UIButton) {
+        let defaults = UserDefaults.standard
+        
+        switch sender.titleLabel?.text
+        {
+        case "Default":
+            defaults.set(String(0), forKey : "sound")
+            sender.backgroundColor = .red
+        case "Bubble":
+            defaults.set(String(1), forKey : "sound")
+            sender.backgroundColor = .red
+        case "Alien":
+            defaults.set(String(2), forKey : "sound")
+            sender.backgroundColor = .red
+        default:
+            break
+        }
     }
-    */
+    
+    @IBAction func cancelPressed(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(String(initialValue), forKey : "color")
+        
+    }
+
 
 }
